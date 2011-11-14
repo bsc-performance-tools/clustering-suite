fe52ac6730c86c2602c749b4efae85ead0ea7f83
Juan González <juan.gonzalez@bsc.es>
2011-11-14 10:53:42 +0000

    Better support for versioning

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+MAJOR=2
+MINOR=1
+sh -c "svnversion -n . | cut -d: -f2 | cut -dS -f1 | tr -d '\n'" > ./SVN_VERSION
+SVN_VERSION=`cat ./SVN_VERSION`
+VERSION="$MAJOR.$MINOR-$SVN_VERSION"
+echo "Current version is $VERSION"
+sed s/@@VERSION_NUMBER@@/$VERSION/ < configure.ac.template > configure.ac
 set -x
 libtoolize --automake --force --copy \
 && aclocal -I config \
