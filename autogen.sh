2793bedf3f22e69b2e501c89eb8ba1a3f5621d3f
Juan González <juan.gonzalez@bsc.es>
2011-11-21 14:53:25 +0000

    Minor version incresed to '2'

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MAJOR=2
-MINOR=1
+MINOR=2
 sh -c "svnversion -n . | cut -d: -f2 | cut -dS -f1 | tr -d '\n'" > ./SVN_VERSION
 SVN_VERSION=`cat ./SVN_VERSION`
 VERSION="$MAJOR.$MINOR-$SVN_VERSION"
